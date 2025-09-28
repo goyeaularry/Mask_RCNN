53979c20b0c233d0b48441e207165d154652a767
goyeaularry <algus.centrum@gmail.com>
2025-09-28 19:12:55 +0200

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -17,6 +17,7 @@ from collections import OrderedDict
 import multiprocessing
 import numpy as np
 import tensorflow as tf
+tf.config.run_functions_eagerly(True)
 import tensorflow.keras as keras
 import tensorflow.keras.backend as K
 import tensorflow.keras.layers as KL
